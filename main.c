56c5e5c7d8f94ddab616251029fd78c8da46a8cc
APAebella <58080899+APAebella@users.noreply.github.com>
2019-11-22 17:08:33 +0100

    LAB04_02 (Vettore Maggioritario)
    Sia dato un vettore vet di N naturali. Si definisce elemento maggioritario, se esiste, quel valore che
    ha numero di occorrenze > N/2.
    Esempio: se N= 7 e vet contiene 3, 3, 9, 4, 3, 5, 3 l’elemento maggioritario è 3. Se N=8 e vet
    contiene 0, 1, 0, 2, 3, 4, 0, 5 non esiste elemento maggioritario.
    Si scriva una funzione maggioritario che, dati N e vet, visualizzi l’elemento maggioritario se
    esiste, -1 se non esiste. Il prototipo sia:
    int majority( int *a, int N);
    Vincoli: complessità O(nlogn), algoritmo in loco.

--- main.c
+++ main.c
@@ -1,194 +1,94 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
-//Codifica dei comandi mediante tipo enum (punto = 0 ... err = 5)
-typedef enum{punto, notquadre, quadre, minusc, maiusc, err}exp;
+int majority(int *a, int N);
 
-char *cercaRegexp(char *src, char *regexp);
-exp isMetaC(char* regexp, int i);
+int maggioritario(int *a, int l, int r);
+
+int contaMaj(int *a, int num, int l, int r);
+
+int main() {
+    int N1 = 7, N2 = 8;
+
+    //Caso elemento maggioritario = 3
+    int v1[7] = {3, 3, 9, 4, 3, 5, 3};
+    int x1 = majority(v1, N1);
+    if (x1 == -1) {
+        printf("Non ci sono elementi maggioritari.\n");
+    } else {
+        printf("L'elemento maggioritario e': %d\n", x1);
+    }
+
+    //Caso in cui non esiste l'elemento maggioritario
+    int v2[8] = {0, 1, 0, 2, 3, 4, 0, 5};
+    int x2 = majority(v2, N2);
+    if (x2 == -1) {
+        printf("Non ci sono elementi maggioritari.\n");
+    } else {
+        printf("L'elemento maggioritario e': %d\n", x2);
+    }
 
 
-int main(){
-    //Stringa sorgente di prova
-    char src[50]="foto noto moto morO amore AmorE Amarena";
-    //Espressione regolare di prova che contiene tutti i metacaratteri (In questo caso l'unica sottostringa valida
-    //all'interno di src è "Amarena"
-    char reg[50]="\\A[^fn].[r]\\a";
-    char *c;
-    //La funzione cercaRegexp ritorna un puntatore ( a carattere ) alla prima occorrenza dell'espressione regolare nella stringa sorgente
-    c=cercaRegexp(src, reg);
-    printf("%s", c);
     return 0;
 }
 
-char *cercaRegexp(char *src, char *regexp){
-    int i,j, lun_src, lun_reg;
-    //Il puntatore a carattere tmp viene inizializzato a NULL in modo da ritornare un puntatore a NULL nel caso in cui la regExp non occorra nella stringa sorgente
-    char * tmp=NULL;
-    int flag, found=0;
-    int count=0;
+int majority(int *a, int N) {
+    int l = 0, r = N - 1;
+    int cnt = 0;
+    int x = maggioritario(a, l, r);
 
-    lun_src=strlen(src);
-    lun_reg=strlen(regexp);
-
-    //Prima ciclo for che itera sulla stringa sorgente e si ferma se raggiunge la fine o se è stato trovata un'occorrenza della regExp
-    for(j=0; j<lun_src && tmp==NULL; j++) {
-
-        flag=1;
-
-        //Secondo ciclo che itera sull'espressione regolare e si ferma se raggiunge la fine o se il flag=0 (Il ciclo finisce prima se
-        for (i = 0; i < lun_reg && flag; i++) {
-
-            //Switch che chiama la funzione isMetaC per controllare se l'elemento in posizione i della regExp è un metacarattere o meno
-            switch (isMetaC(regexp, i)) {
-                //Se l'i-esimo carattere della regExp è un punto ogni carattere della stringa sorgente va bene, quindi si
-                //aggiorna la variabile count ( trovata una corrispondenza tra stringa e regExp ) e se la variabile tmp = NULL
-                //questa diventa il puntatore al j-esimo carattere della stringa sorgente [Prima occorrenza della regExp nella stringa sorgente]
-                case punto:
-                    if(tmp==NULL)
-                        tmp=&src[j];
-                    count++;
-                    break;
-
-                //Se viene trovato il metacarattere [...] qualsiasi carattere all'interno delle parentesi è accettabile
-                //all'interno della stringa, quindi viene posto il flag found = 0 e un ciclo scorre a partire dalla prima
-                //parentesi sino alla parentesi chiusa, controllando per ogni elemento se questo è uguale al j+count-esimo
-                //carattere della stringa sorgente. In caso positivo found = 1 e il controllo fuori dal ciclo aggiornerà count
-                //e assegnerà a tmp il j-esimo [prima occorrenza] carattere della stringa sorgente se tmp=NULL [ cioè se
-                //non sono state trovate altre occorrenze prima di quel momento. In caso negativo (found = 0) a tmp verrà
-                //assegnato NULL e count tornerà al valore 0.
-                case quadre:
-
-                    i++; //Salta il carattere '[' nella regexp
-                    found=0;
-                    while (regexp[i] != ']') {
-                        if(src[j+count]==regexp[i])
-                            found = 1;
-                        i++;
-                    }
-
-                    if(found){
-                        if(tmp==NULL)
-                            tmp=&src[j];
-                        count++;
-                    } else {
-                        tmp=NULL;
-                        count=0;
-                    }
-                    break;
-
-                //Se viene trovato il metacarattere [^...] nessun carattere all'interno delle parentesi è accettabile
-                //all'interno della stringa, quindi viene posto il flag found = 0 e un ciclo scorre sino al carattere ']'
-                //nella regexp controllando se il j+count-esimo carattere della stringa sorgente sia uguale all'i-esimo
-                //carattere della regexp. In caso positivo il flag found = 1, cioè è stato trovato un carattere che non è
-                //accettabile. Se alla fine del ciclo found = 1 al puntatore tmp viene assegnato NULL e count ritorna = 0
-                //mentre in caso contrario si aumenta count e se tmp = NULL gli viene assegnato il puntatore al j-esimo carattere
-                //nella regexp.
-                case notquadre:
-
-                    i+=2; //salta i caratteri '[^' nella regexp
-                    found=0;
-                    while (regexp[i] != ']') {
-                        if(src[j+count]==regexp[i])
-                            found=1;
-                        i++;
-                    }
-
-                    if(found){
-                        tmp=NULL;
-                        count=0;
-                    } else {
-                        if(tmp==NULL)
-                            tmp=&src[j];
-                        count++;
-                    }
-                    break;
-
-                //Se viene trovato il metacarattere \a qualsiasi carattere minuscolo è accettabile. In questo caso si
-                //controlla che il j+count-esimo carattere della stringa sorgente sia minuscolo tramite islower(). In caso
-                //positivo si aggiorna count e se tmp = NULL gli viene assegnato il puntatore al j-esimo carattere della
-                //stringa sorgente. In caso negativo a tmp viene assegnato il puntatore NULL e count ritorna 0.
-                case minusc:
-                    i++; //salta il carattere '\a' [considerato come carattere singolo]
-                    if(islower(src[j+count])){
-                        if(tmp==NULL)
-                            tmp=&src[j];
-                        count++;
-                    } else{
-                        tmp=NULL;
-                        count=0;
-                    }
-
-                    break;
-
-                //Analogo al caso minusc
-                case maiusc:
-                    i++;
-                    if(isupper(src[j+count])){
-                        if(tmp==NULL)
-                            tmp=&src[j];
-                        count++;
-                    } else{
-                        tmp=NULL;
-                        count=0;
-                    }
-                    break;
-
-                //Se l'i-esimo carattere di regexp non è un metacarattere (carattere standard o spazio) si controlla che
-                //il j+count-esimo elemento della stringa sorgente sia uguale all'i-esimo elemento della regexp. In caso
-                //positivo si aggiorna count e se tmp = NULL gli viene assegnato il puntatore al j-esimo carattere della
-                //stringa sorgente. In caso negativo a tmp viene assegnato NULL e count ritorna a 0.
-                default:
-                    if(src[j+count]==regexp[i]){
-                        if(tmp==NULL)
-                            tmp=&src[j];
-                        count++;
-                    } else{
-                        tmp=NULL;
-                        count=0;
-                    }
-                    break;
-            }
-
-            //Se dopo lo switch (controllo dei metacaratteri) il puntatore tmp = NULL allora flag e count ritornano uguali
-            //a 0. In caso contrario flag viene posto = 1 (Espressione trovata all'interno della stringa).
-            if(tmp==NULL){
-                flag=0;
-                count=0;
-            } else {
-                flag=1;
-            }
+    //La funzione maggioritario ritorna solo il numero che ha più occorrenze nel vettore, quindi è necessario scorrerlo (al massimo) tutto per vedere se questo è l'elemento maggioritario
+    for (int i = 0; i < N; i++) {
+        if (x == a[i]) {
+            cnt++;
+        }
+        if (cnt > N / 2) {
+            return x;
         }
     }
 
-    //La funzione ritorna al programma chiamante la variabile tmp che sarà NULL o il puntatore alla prima occorrenza di
-    //regexp all'interno della stringa sorgente.
-    return tmp;
+    //Se non ci sono elementi maggioritari la funzione ritorna -1
+    return -1;
+
 }
 
-exp isMetaC(char* regexp, int i){
-    //Viene creata una tabella di 5 elementi contenente i metacaratteri (o una parte nel caso delle quadre)
-    char *caratteri[err]={".", "[^", "[", "\\a", "\\A"};
-    //Alla variabile meta di tipo enum exp viene assegnato err (5) che corrisponde a tutto ciò che non è un metacarattere
-    exp meta=err;
-    int j;
+int maggioritario(int *a, int l, int r) {
 
-    //Se l'i-esimo carattere di regexp non è alfanumerico allora si controlla per ogni metacarattere che questo appaia
-    //esattamente nell'i-esimo carattere della regexp [si controlla che la prima occorrenza del metacarattere si trovi a partire
-    //da l'i-esimo carattere della regexp e non dopo (risolve il caso in cui ci siano più metacaratteri nella regexp) ]
-    if(!isalnum(regexp[i]))
-        for(j=0; j<err; j++){
-            if(strstr(regexp+i, caratteri[j])==regexp+i)
-                return (meta=j);
+    //Caso semplice: in un vettore da un solo elemento, questo sarà quello maggioritario
+    if (l == r) {
+        return a[l];
+    }
+
+    //Ricorsione sul sottovettore sinistro e destro
+    int q = (l + r) / 2;
+    int left = maggioritario(a, l, q);
+    int right = maggioritario(a, q + 1, r);
+
+    //Se le due metà hanno lo stesso elemento maggioritario lo ritornano
+    if (left == right) {
+        return left;
+    }
+
+    //In caso contrario conta quanti elementi maggioritari ci sono nel sottovettore sinistro e quanti nel sottovettore destro
+    int ContaLeft = contaMaj(a, left, l, r);
+    int ContaRight = contaMaj(a, right, l, r);
+
+    //Ritorna il vincitore tra i due possibili maggioritari(se hanno lo stesso numero di occorrenze ritorna il primo che ha trovato, cioè il sinistro)
+    return ContaLeft >= ContaRight ? left : right;
+
+}
+
+int contaMaj(int *a, int num, int l, int r) {
+    int i, cnt = 0;
+    //Conta quante volte occorre il presunto elemento maggioritario all'interno del sottovettore
+    for (i = l; i <= r; i++) {
+        if (a[i] == num) {
+            cnt++;
         }
-
-    return meta;
-
+    }
+    return cnt;
 }
-
 
 //
-// Created by Gabriele Cirotto on 04/11/2019.
+// Created by Gabriele Cirotto on 02/11/2019.
 //
 
